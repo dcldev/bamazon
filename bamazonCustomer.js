d4b4c8293829d1498e5db7a41ec089b5d4ab5c69
Han Seoulo <cu.rogue.coder@gmail.com>
2019-04-02 18:29:52 -0400

    testing out npm http module and converting sql to json

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const http = require("http");
 
 let connection = mysql.createConnection({
     host: "localhost",
@@ -9,8 +10,23 @@ let connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
+
+const server = http.createServer((req, res) => {
+    console.log('Someone accessed my server!');
+});
+
+server.listen(3306, '127.0.0.1', () => {
+    console.log('Listening for requests');
+})
+
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     //run BamazonSearch function goes here
 });
+
+function bamazonCustomerSearch () {
+    prompt([
+        
+    ])
+}
