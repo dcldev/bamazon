4cee76aeaefeeaa8fc24ce720f57b750e91bf069
Han Seoulo <cu.rogue.coder@gmail.com>
2019-04-02 16:16:29 -0400

    created server and connection function

---
+++ bamazonCustomer.js
@@ -0,0 +1,16 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    //run BamazonSearch function goes here
+});
