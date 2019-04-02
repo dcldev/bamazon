d89cb7c05f4e8613ad5a72f6a8ebc743352dec4f
Han Seoulo <cu.rogue.coder@gmail.com>
2019-04-02 16:09:26 -0400

    correct mysql syntax for "decimal" p & d values

--- bamazon.sql
+++ bamazon.sql
@@ -8,7 +8,7 @@ CREATE TABLE products (
   item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(45) NULL,
   department_name VARCHAR(45) NULL,
-  price DECIMAL(10,2) NULL,
+  price DECIMAL(65,2) NULL,
   stock_quantity INT NULL,
   PRIMARY KEY (item_id)
 );
