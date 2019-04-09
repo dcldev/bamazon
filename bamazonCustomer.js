aea771be157528681546368fa9f8709516cbec17
Han Seoulo <cu.rogue.coder@gmail.com>
2019-04-09 16:10:26 -0400

    initializing prompt for prod selection and quanity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
-const http = require("http");
+let colors = require("colors/safe");
 let fancyTable = require("cli-table");
 
 let connection = mysql.createConnection({
@@ -46,60 +46,81 @@ function promptCustomer() {
             switch (answers.action) {
                 case "PURCHASE AN ITEM":
                     displayTable(products);
-                    // beginOrder();
+                    break;
+                case "EXIT":
+                    console.log("Come again, you must!");
+                    connection.end();
                     break;
             }
-
-        })
+        });
 }
 
 
 function displayTable() {
     let query = connection.query("SELECT * from products", function (err, res) {
-    let drawTable = new fancyTable({
-        head: ["ID", "PRODUCT NAME", "DEPARTMENT NAME", "PRICE", "QTY"]
-    });
+        let drawTable = new fancyTable({
+            head: ["ID", "PRODUCT NAME", "DEPARTMENT NAME", "PRICE", "QTY"]
+        });
 
-    for (var i = 0; i < res.length; i++){
-        drawTable.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity])
-    }
+        for (var i = 0; i < res.length; i++) {
+            drawTable.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity])
+        }
 
-    console.log(drawTable.toString());
+        console.log(drawTable.toString());
+        yourMoveKimosabe();
 
-})
+    })
 
 }
 
+function yourMoveKimosabe() {
+    inquirer
+        .prompt([{
+            name: "item",
+            type: "number",
+            message: "Enter the ITEM ID for the ITEM you want to PURCHASE:",
+            min: 1
+        },
+            {
+            name: "quantity",
+            type: "number",
+            message: "Enter the QUANTITY of the AMOUNT you want to PURCHASE:",
+            min: 1,
+            }
+        ])
+        .then( answer => {
+        }
+        )
+    };
 
-    // products.forEach((product, i) => {
-    //     console.log(i === product.length);
-    //       drawTable.push([product.item_id, product.product_name, product.department_name, product.price, product.stock_quantity]);
-    //       console.log(drawTable.toString());
-    //     });
-    //   });
-    // };
+// products.forEach((product, i) => {
+//     console.log(i === product.length);
+//       drawTable.push([product.item_id, product.product_name, product.department_name, product.price, product.stock_quantity]);
+//       console.log(drawTable.toString());
+//     });
+//   });
+// };
 
-    // products.forEach((product) => {
-    //     product.sizes.forEach((size) => {
-    //       console.log(size);
-    //     });
-    //   });
-    
-
-    // connection.query(query,
-    //         function (err, res) {
-    //         products.forEach(function(res, i)
-            
+// products.forEach((product) => {
+//     product.sizes.forEach((size) => {
+//       console.log(size);
+//     });
+//   });
 
 
-    //         }
-    //         for (var i = 0; i < res.length; i++)
-    //             if (err) {
-    //                 console.log("There has been an error unable to gather products");
-    //             }
-    //             console.log(res);
-    //             drawTable.push(res:[{product_name, department_name, price, stock_quantity}]);
-    //             console.log(drawTable.toString());
-                
-    //         })
-            
+// connection.query(query,
+//         function (err, res) {
+//         products.forEach(function(res, i)
+
+
+
+//         }
+//         for (var i = 0; i < res.length; i++)
+//             if (err) {
+//                 console.log("There has been an error unable to gather products");
+//             }
+//             console.log(res);
+//             drawTable.push(res:[{product_name, department_name, price, stock_quantity}]);
+//             console.log(drawTable.toString());
+
+//         })
